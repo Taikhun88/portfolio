ecf66ffc9374120496a0f4913a6dd7f2b10ece25
Taikhun88 <taiyang78190@gmail.com>
2021-04-20 11:41:19 +0200

    save data changes

---
+++ myprojects/src/components/Nav/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from './Nav.module.css';
+
+function Nav() {
+    return (
+        <nav className = {classNames.navbar}>
+            <div>            
+                <img src="logo192.png" alt="logo site" className = {classNames.imgLogo}/>
+           
+            </div>
+            <div>
+                <ul>
+                    <li>Mes projets</li>
+                    <li>Se connecter</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
+
