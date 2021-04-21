b24ff93f082a05f71f677a604bc74cf347d54ba9
NaimAftab <naima.aftab@reseau.sncf.fr>
2021-04-21 10:28:17 +0200

    Creation card

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import Cards from '../components/Cards/Cards';
 import Nav from '../components/Nav/Nav';
 import Carousel from '../components/Carousel/Carousel';
+import Card from '../components/Cards/Card';
+import classNames from './Home.module.css'
 
 function Home() {
     return (
         <div>
             <Nav/>
             <Carousel/>
+            <div className = {classNames.divContainer}>
+            <Card/>
+            <Card/>
+            <Card/>
+            </div>
             <Cards />
 
                         
